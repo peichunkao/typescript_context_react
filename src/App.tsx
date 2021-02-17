a14e132a97b92a728e8b336355e36d9d2157747f
Steven Kao <stevenkaoo@gmail.com>
2021-02-17 15:59:16 +1100

    add typescript to react app

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Store } from './Store'
+import { IAction,IEpisode } from './interface'
+
+
+function App (): JSX.Element {
+  const { state, dispatch } = React.useContext(Store)
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+
+  const toggleFavAction = (episode: IEpisode) : IAction => dispatch({
+    type: 'ADD_FAV',
+    payload: episode
+  })
+
+  return (
+    <React.Fragment>
+      <header className="header">
+        <h1>Rick and Morty</h1>
+        <p>Pick your favorite episode!</p>
+      </header>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id} className="episode-box">
+              <img
+                src={episode.image.medium}
+                alt={`Rick and Mort ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                <div>Season: {episode.season} Number: {episode.number}</div>
+                <button type='button' onClick={()=>toggleFavAction(episode)}>Fav</button>
+              </section>
+            </section>
+          )
+        })}
+      </section>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/interface.tsx
@@ -0,0 +1,28 @@
+export interface IState {
+    episodes:Array<IEpisode>,
+    favorites:Array<any>
+}
+
+export interface IAction {
+    type:string,
+    payload:any
+}
+
+export interface IEpisode {
+    id: number
+    url: string
+    name: string
+    season: number
+    number: number
+    type: string
+    airdate: string
+    airtime: string
+    airstamp: string
+    runtime: number
+    image: {
+      medium: string
+      original: string
+    }
+    summary: string
+  }
+  
